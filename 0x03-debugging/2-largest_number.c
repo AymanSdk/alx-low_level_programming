ce35dd41cd3ef910a2b63ec8d5db7d3f395540ed
AymanSdk <ayman.sdk123@outlook.com>
2023-02-22 04:32:17 -0800

    largest number

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a >= b && a >= c)
+	{
+		largest = a;
+	}
+	else if (b >= a && b >= c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
