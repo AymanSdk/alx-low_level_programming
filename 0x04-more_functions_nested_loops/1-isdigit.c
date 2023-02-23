2360231fd068b6570b32a98943977f610571bf99
AymanSdk <ayman.sdk123@outlook.com>
2023-02-23 03:11:24 -0800

    check digits with _isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isdigit - write a function to check for a digit (0 through 9)
+ * @c: 0 or 1
+ * Return: 0 or 1
+ */
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '9')
+		return (1);
+	else 
+		return (0);
+}
