0f5e7eb472f8dd236d94c51099b40c9c27989891
AymanSdk <ayman.sdk123@outlook.com>
2023-06-16 15:15:15 +0100

    Print Number From 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Prints all single digit numbers of base 10 starting from 0.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int N;
+
+	for (N = 0 ; N < 10 ; N++)
+	{
+		printf("%d", N);
+	}
+	printf("\n");
+
+	return (0);
+
+}
