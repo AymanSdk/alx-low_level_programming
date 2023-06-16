7ce78a9c895133fa581d4496324e0b0bb97e465d
AymanSdk <ayman.sdk123@outlook.com>
2023-06-16 12:44:36 +0100

    Last Digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	f = n % 10;
-	if (f < 6 && 1 != 0)
+	if (f < 6 && f != 0)
 	{
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, f);
 	}
