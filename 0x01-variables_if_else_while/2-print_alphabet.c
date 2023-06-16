303a87498227aaf2dbfdabe612337b1a81e371c7
AymanSdk <ayman.sdk123@outlook.com>
2023-06-16 14:33:22 +0100

    LowerCase Alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - main block
+ *
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+	char alphastart = 'a';
+
+	while (alphastart < 'z')
+	{
+		putchar(alphastart);
+		alphastart++;
+	}
+	return (0);
+}
