4e535d22bac9bf62c4bc912a33b11111585ed3b7
AymanSdk <ayman.sdk123@outlook.com>
2023-06-20 18:39:50 +0100

    15 Times Table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,43 +1,46 @@
 #include "main.h"
+
 /**
  * print_times_table - prints the n times table, starting with 0.
  * @n: The number of times table
  *
  * Return: void
  */
+
 void print_times_table(int n)
 {
-	int si7ri, bondoq, ha9od;
+	int si7ri, bondo9, hawli;
 
-	if (n >= 0 && n <= 15)
+	if( n >= 0 && n <= 15)
 	{
 		for (si7ri = 0; si7ri <= n; si7ri++)
 		{
-			_putchar('0');
-			for (bondoq = 1; bondoq <= n; bondoq++)
+			_putchar(48);
+			for (bondo9 = 1; bondo9 <= n; bondo9++)
 			{
-				_putchar(',');
-				_putchar(' ');
-				ha9od = si7ri * bondoq;
-				if (ha9od <= 9)
+				hawli = si7ri * bondo9;
+				_putchar(44);
+				_putchar(32);
+				if (hawli <= 9)
 				{
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(ha9od + '0');
+					_putchar(32);
+					_putchar(32);
+					_putchar(hawli + 48);
 				}
-				else if (ha9od <= 99)
+				else if (hawli <= 99)
 				{
-					_putchar(' ');
-					_putchar(ha9od / 10 + '0');
-					_putchar(ha9od % 10 + '0');
+					_putchar(32);
+					_putchar((hawli / 10) + 48);
+					_putchar((hawli % 10) + 48);
 				}
 				else
 				{
-					_putchar(ha9od / 100 + '0');
-					_putchar(ha9od / 10 % 10 + '0');
-					_putchar(ha9od % 10 + '0');
+					_putchar(((hawli / 100) % 10) + 48);
+					_putchar(((hawli / 10) % 10) + 48);
+					_putchar((hawli % 10) + 48);
 				}
 			}
 			_putchar('\n');
 		}
 	}
+}
