25da9b45056b7e01f7dc06d849e991e8d05dc251
AymanSdk <ayman.sdk123@outlook.com>
2023-06-22 14:19:09 +0100

    Draw Diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
+ *
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int i, t;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (t = 0; t < i; t++)
+				_putchar(' ');
+			_putchar('\\');
+			if (i == n - 1)
+				continue;
+			_putchar('\n');
+		}
+	}
+
