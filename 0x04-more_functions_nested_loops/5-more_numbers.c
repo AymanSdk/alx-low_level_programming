b7129cad9a0cae9a8414d054f4c48757d1d2ff1c
AymanSdk <ayman.sdk123@outlook.com>
2023-06-22 13:07:07 +0100

    More Numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers, from 0 to 14
+ *
+ * Return: void
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (j = 0; j < 15; ++j)
+		{
+			if (j >= 10)
+				_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
