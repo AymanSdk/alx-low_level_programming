3473601cb16e72712a8fba8139cf211d2394a97f
AymanSdk <ayman.sdk123@outlook.com>
2023-06-22 17:41:16 +0100

    clear

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_number - prints an integer
+ * @n: integer to be printed
+ *
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+		num = n;
+
+	if (num / 10)
+		print_number(num / 10);
+
+	_putchar(num % 10 + '0');
+}
