36859d017208f925df749d4d54d8508427187f6f
AymanSdk <ayman.sdk123@outlook.com>
2023-07-06 00:24:25 +0100

    Power

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ * @x: base number
+ * @y: power number
+ * Return: x raised to the power of y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int result = x;
+
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else
+		result *= _pow_recursion(x, y - 1);
+	return (result);
+}
