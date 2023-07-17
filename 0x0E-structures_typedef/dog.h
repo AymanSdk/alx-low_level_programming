2afcd830e9ba648cc001101016fad31825d222df
AymanSdk <ayman.sdk123@outlook.com>
2023-07-17 23:56:04 +0100

    DOG_H HEADER

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,29 @@
+#ifndef	DOG_H
+#define	DOG_H
+
+/**
+ *  struct dog - dog structure
+ *  @name: dog's name
+ *  @age: dog's age
+ *  @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+/**
+ *  dog_t - typedef for struct dog
+ */
+
+typedef struct dog dog_t;
+
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
+#endif
