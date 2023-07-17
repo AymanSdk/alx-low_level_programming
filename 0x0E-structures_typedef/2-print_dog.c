bdcdd5c6f421c81ca2de639561a68cfcced17a74
AymanSdk <ayman.sdk123@outlook.com>
2023-07-18 00:08:02 +0100

    DOGGY

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
 
+/**
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog
+ *
+ * Return: void
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d != NULL)
+	{
+		printf("Name: %s\n", (d->name == NULL) ? "(nil)" : d->name);
+		printf("Age: %f\n", d->age);
+		printf("Owner: %s\n", (d->owner == NULL) ? "(nil)" : d->owner);
+	}
+}
