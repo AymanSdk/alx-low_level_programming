383911ed6da89d3c59e14c18e361c211eb255ca5
AymanSdk <ayman.sdk123@outlook.com>
2023-07-19 21:55:53 +0100

    header Update II

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,5 +1,5 @@
-#ifndef "FUNCTION_POINTERS_H"
-#define "FUNCTION_POINTERS_H"
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
