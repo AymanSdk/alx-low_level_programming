1dc1b3a8658ee84f12e7b49885a7e2ba10bcb90f
AymanSdk <ayman.sdk123@outlook.com>
2023-07-20 18:57:23 +0100

    sum them all

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,24 +1,27 @@
+#include <tdarg.h>
 #include "variadic_functions.h"
-#include <stdarg.h>
 
 /**
- * sum_them_all - returns the sum of all its parameters
- * @n: number of parameters
+ * sum_them_all -> Function that returns the sum of all its parameters.
  *
- * Return: sum of all parameters
+ * @n: Input Number
+ *
+ * Return: Depennd Condition
  */
 
 int sum_them_all(const unsigned int n, ...)
 {
 	unsigned int i;
 	int sum = 0;
-	va_list args;
+	va_list list;
 
-	va_start(args, n);
+	va_start(list, n);
 	if (n == 0)
 		return (0);
-	for (i = 0; i < n; ++i)
-		sum += va_arg(args, int);
-	va_end(args);
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(list, const unsigned int);
+	}
+	va_end(list);
 	return (sum);
 }
