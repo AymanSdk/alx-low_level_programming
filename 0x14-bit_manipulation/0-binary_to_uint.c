aeb41b8b4f00a023fc3098aedfcdd8de2eafb66e
AymanSdk <ayman.sdk123@outlook.com>
2023-08-04 00:23:55 +0100

    Binary to Unsigned

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * binary_to_uint -converts a binary to an unsigned int.
+ * @b: point to a string of 0 and 1 chars
+ *
+ * Return: the converted number, or 0
+ *
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int j;
+
+	j = 0;
+	if (!b)
+		return (0);
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		if (b[i] != '0' && b[i] != '1')
+			return (0);
+	}
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		j <<= 1;
+		if (b[i] == '1')
+			j += 1;
+	}
+	return (j);
+}
