7e43b216fb0f09b29f775645bb42d0365aa1a455
AymanSdk <ayman.sdk123@outlook.com>
2023-08-04 20:39:37 +0100

    fixing betty

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <stdio.h>
 /**
  * _power - calculate the (base and power)
  * @base: base of exponet
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,5 +1,7 @@
-#ifndef MIN_H
+#ifndef MAIN_H
 #define MAIN_H
+
+
 unsigned long int _power(unsigned int base, unsigned int pow);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
