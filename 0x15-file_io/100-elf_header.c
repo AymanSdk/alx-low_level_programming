e7429dda8ed8d1fd91f9f32d033d303258cf6c8e
AymanSdk <ayman.sdk123@outlook.com>
2023-08-07 22:34:27 +0100

    test again

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,4 +1,4 @@
-#include "ain.h"
+#include "main.h"
 
 /**
  * main - Entry point
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,4 +1,4 @@
-#inlude "main.h"
+#include "main.h"
 /**
  * main - copies the content of a file to another file
  * @argc: number of arguments
